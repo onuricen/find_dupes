0876a2bc6d6ff9c4d4624c2b0830a452a4e14b41
Onur <icenonur@gmail.com>
2020-07-22 14:57:39 +0300

    initial commit

---
+++ dosya.py
@@ -0,0 +1,66 @@
+import os, sys
+import hashlib
+ 
+def findDup(parentFolder):
+    dups = {}
+    for dirName, subdirs, fileList in os.walk(parentFolder):
+        print('Taranıyor %s...' % dirName)
+        for filename in fileList:
+            path = os.path.join(dirName, filename)
+            file_hash = hashfile(path)
+            if file_hash in dups:
+                dups[file_hash].append(path)
+            else:
+                dups[file_hash] = [path]
+    return dups
+ 
+ 
+
+def joinDicts(dict1, dict2):
+    for key in dict2.keys():
+        if key in dict1:
+            dict1[key] = dict1[key] + dict2[key]
+        else:
+            dict1[key] = dict2[key]
+ 
+ 
+def hashfile(path, blocksize = 65536):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+ 
+ 
+def printResults(dict1):
+    results = list(filter(lambda x: len(x) > 1, dict1.values()))
+    if len(results) > 0:
+        print('Aynı dosyalar bulundu:')
+        print('Aşağıdaki dosyalar aynı')
+        print('___________________')
+        for result in results:
+            for subresult in result:
+                print('\t\t%s' % subresult)
+            print('___________________')
+ 
+    else:
+        print('Aynı dosya bulunmadı.')
+ 
+ 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        dups = {}
+        folders = sys.argv[1:]
+        for i in folders:
+            
+            if os.path.exists(i):
+                joinDicts(dups, findDup(i))
+            else:
+                print('%s düzgün bir dizin değil ' % i)
+                sys.exit()
+        printResults(dups)
+    else:
+        print('Kullanım python dosya.py (dosya_adi)')
